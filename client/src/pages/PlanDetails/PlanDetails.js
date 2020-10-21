76aed1f2cb449b151dad4c22da2359c81728b870
nick-ramsay <NickRamsay1989@gmail.com>
2020-10-21 19:00:59 +1100

    Added more detail and modal to task details

--- client/src/pages/PlanDetails/PlanDetails.js
+++ client/src/pages/PlanDetails/PlanDetails.js
@@ -68,23 +68,40 @@ const PlanDetails = () => {
                     <div className="col-md-12 bg-white p-2">
                         <h2><strong>{'"' + Plan.plan_name + '"'}</strong></h2>
                         <h4>{moment(Plan.created_date).format("dddd,  DD MMMM YYYY")}</h4>
-                        <form className="mt-3">
-                            <div className="form-row text-center">
-                                <div className="col">
-                                    <input type="text" placeholder="Enter your task here" className="form-control" id="taskInput" name="taskInput" onChange={setNewTask} aria-describedby="taskHelp" />
+                        <button type="button" className="btn btn-custom" data-toggle="modal" data-target="#newTaskModal">
+                            New Task
+                        </button>
+                        <div class="modal fade" id="newTaskModal" tabindex="-1" aria-labelledby="newTaskModalLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="newTaskModalLabel">Enter a New Task</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <form className="mt-3">
+                                            <div className="form-row text-center">
+                                                <div className="col">
+                                                    <input type="text" placeholder="Enter your task description here" className="form-control" id="taskInput" name="taskInput" onChange={setNewTask} aria-describedby="taskHelp" />
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    <div className="modal-footer">
+                                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button type="button" className="btn btn-custom" onClick={saveTask}>Save changes</button>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="form-row text-center">
-                                <div className="col mt-3">
-                                    <div type="button" className="btn btn-custom" tabIndex="0" onClick={saveTask}>Save Task</div>
-                                </div>
-                            </div>
-                        </form>
+                        </div>
                         {/*<div className="btn btn-sm btn-custom-red mb-1 mt-1" data-plan_id={PlanID} onClick={deletePlan}>Delete Task</div>*/}
                         <div>
                             <p>{Plan.tasks != undefined ? Plan.tasks.map((task, i) =>
                                 <div className="card mb-1 mt-1 p-2 text-left">
-                                    <span>{(i+1) + ". " + task.description}</span>
+                                    <h4>{(i + 1) + ". " + task.description + "  "}{task.status === "Closed" ? <span class="badge badge-success">{task.status}</span>:<span class="badge badge-warning">{task.status}</span>}</h4>
+                                    <p>{moment(task.created_date).format("DD MMMM YYYY, h:mm A")}</p>
                                 </div>
                             ) : ""}</p>
                         </div>
