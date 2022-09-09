296058dff51830e054fc8a668ce874c48872f642
nick-ramsay <nick.ramsay@datadoghq.com>
2022-09-09 14:26:39 +1000

    Adding version, env, service tags to RUM

---
+++ client/src/webpack.config.js
@@ -0,0 +1,18 @@
+const webpack = require('webpack');
+
+module.exports = {
+  mode: 'production',
+  devtool: false,
+  plugins: [
+    new webpack.SourceMapDevToolPlugin({
+      noSources: false,
+      filename: '[file].map'
+    }),
+    // ...
+  ],
+  optimization: {
+    minimize: true,
+    // ...
+  },
+  // ...
+};
