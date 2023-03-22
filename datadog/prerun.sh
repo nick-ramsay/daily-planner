c407e07f39439689305e5fd688458e5914476381
nick-ramsay <nick.ramsay@datadoghq.com>
2023-03-22 14:20:16 +1100

    Fixing prerun script

--- datadog/prerun.sh
+++ datadog/prerun.sh
@@ -7,6 +7,6 @@
 
 # Set app version based on HEROKU_SLUG_COMMIT
 DD_VERSION="Default"
-if [ -n "$HEROKU_SLUG_COMMITT" ]; then
+if [ -n "$HEROKU_SLUG_COMMIT" ]; then
     DD_VERSION=$HEROKU_SLUG_COMMIT
 fi
