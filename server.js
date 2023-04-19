6bd2538a828af905cf547771d170806b59422e85
nick-ramsay <nick.ramsay@datadoghq.com>
2023-04-19 15:19:04 +1000

    Enabled debug mode, dd-trace

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
+const bunyan = require('bunyan')
+const logger = bunyan.createLogger({
+  name: 'dd-trace',
+  level: 'trace'
+})
+
 //Datadog Tracing Injections
 const tracer = require('dd-trace').init({
   debug: true,
